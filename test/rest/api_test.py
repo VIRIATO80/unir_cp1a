392ce10b76192b0e996633a373a3097e77622f72
Javier Lindo <javi.lindo@gmail.com>
2023-12-09 11:17:21 +0100

    error en el test de la división

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -52,7 +52,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "4", "ERROR DIVIDE"
+            response.read().decode(), "2", "ERROR DIVIDE"
         )
 
     def test_api_sqrt(self):
